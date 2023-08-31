92b0bd221be523e7ba9b4e9673a16482fd1f7fcd
Donald duck <tang5722917@163.com>
2023-08-31 20:20:01 +0800

    为Lua 添加Object支持 添加系统定时器/send

--- mudlet/config.lua
+++ mudlet/config.lua
@@ -11,3 +11,16 @@ Copyright (c) 2023 by Donald duck email: tang5722917@163.com, All Rights Reserve
 
 --调试log输出等级 0最低 5最高
 Debug_log_rank = 5
+
+
+-- 系统时钟设置
+fast_time = 0.1      --0.1s  系统处理节拍
+normal_time = 0.5    --0.5  一般send指令节拍
+slow_time = 1        --1s  系统处理节拍
+slow_time1 = 60      --1min  监测节拍
+slow_time2 = 600     --10min  监测节拍
+slow_time3 = 3600 --1h  监测节拍
+
+
+-- UI 设置
+HP_UI_Auto_time = 60  --UI HP 数据自动刷新时间
--- mudlet/init.lua
+++ mudlet/init.lua
@@ -4,7 +4,7 @@ Date: 2023-08-16 15:08:55
 LastEditors: Donald duck tang5722917@163.com
 LastEditTime: 2023-08-16 15:09:06
 FilePath: \Auto_test_tool\mudlet\init.lua
-Description: lua文件导入
+Description: lua文件UI
 Copyright (c) 2023 by Donald duck email: tang5722917@163.com, All Rights Reserved.
 --]]
 -- 定义重启事件
@@ -13,11 +13,23 @@ function ReloadEven(event)
     SysReset()
     dofile(Lua_path..'init.lua')
 end
-Con_path = Test_path.."MUD/"
+Con_path = Test_path .. "MUD/"
 --Lua_path LUA 环境目录
+
+dofile(Lua_path .. "Object/SysObject.lua")
+dofile(Lua_path .. "Object/Trigger_Base.lua")
+dofile(Lua_path .. "Object/Complex_Trigger.lua")
+dofile(Lua_path .. "Object/SysFun_table.lua")
+dofile(Lua_path .. "Object/Task_Base.lua")
+dofile(Lua_path .. "Object/Task_Control.lua")
+dofile(Lua_path .. "Object/Timer_Base.lua")
+
 dofile(Lua_path .. "config.lua")
 dofile(Lua_path .. "sys/SysLog.lua")
 dofile(Lua_path .. "sys/SysKey.lua")
 dofile(Lua_path .. "sys/SysReset.lua")
+dofile(Lua_path .. "sys/sys_init.lua")
+--导入UI文件
+dofile(Lua_path .. "UI/UI_init.lua")
 
 dofile(Lua_path .. "main.lua")
